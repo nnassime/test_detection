98b18d0d645f063ed6944174815ba29cd2fcaf7b
A. Belaid <62215850+nnassime@users.noreply.github.com>
2021-11-17 12:30:14 +0100

    Update streamlit_app.py

--- streamlit_app.py
+++ streamlit_app.py
@@ -67,7 +67,7 @@ def starter():
 
 
 def prediction(vname):
-    os.system("python -m pip install numpy torch pandas")
+    os.system("python -m pip install numpy torch pandas PIL")
     vpath='data/'+vname
     wpath = 'yolov5/weights/crowdhuman_yolov5m.pt'
     if os.path.exists(wpath):
