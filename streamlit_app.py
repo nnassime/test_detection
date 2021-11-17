77b7d31a86898fe119ec1c85162a25c7b846e6ce
A. Belaid <62215850+nnassime@users.noreply.github.com>
2021-11-17 12:23:32 +0100

    Update streamlit_app.py

--- streamlit_app.py
+++ streamlit_app.py
@@ -67,7 +67,7 @@ def starter():
 
 
 def prediction(vname):
-    os.system("python3 pip install -r numpy")
+    os.system("python pip install -r numpy")
     vpath='data/'+vname
     wpath = 'yolov5/weights/crowdhuman_yolov5m.pt'
     if os.path.exists(wpath):
