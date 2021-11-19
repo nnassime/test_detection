63d4a56c9d36b1c92ec4af03b0cbbec269a3cc9a
A. Belaid <62215850+nnassime@users.noreply.github.com>
2021-11-19 15:41:01 +0100

    Update experimental.py

--- yolov5/models/experimental.py
+++ yolov5/models/experimental.py
@@ -9,8 +9,9 @@ import torch.nn as nn
 
 import sys
 
-sys.path.insert(0, 'home/user/app/test_detection/yolov5/models')
+sys.path.insert(0, 'app/test_detection/yolov5/models')
 sys.path.insert(0, 'test_detection/yolov5/models')
+sys.path.insert(0, 'test_detection/yolov5')
 
 
 from models.common import Conv
